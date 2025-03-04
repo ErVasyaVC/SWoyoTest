9ab43c31f7c7056988dd94a8c482c2d50d9ab9df
ErVasyaVC <ervasyavc@gmail.com>
2025-03-04 18:06:11 +0300

    create cli file

---
+++ cli.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def cli()-> (int, int, str):
+    parser = argparse.ArgumentParser(description="Send sms")
+    parser.add_argument('send_num', type=int, help="Sender number")
+    parser.add_argument('rec_num', type=int, help="Recipient number")
+    parser.add_argument('text_sms', type=str, help="Text SMS")
+    args = parser.parse_args()
+    print(args.send_num)
+    print(args.rec_num)
+    print(args.text_sms)
+
+    return args.send_num, args.rec_num, args.text_sms
