d36c160bfdee733cb7160d6e4d0a0537e5d6e609
ErVasyaVC <ervasyavc@gmail.com>
2025-03-09 22:40:03 +0300

    update

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,11 @@
 import logging
+from  config_loader import load_config
 
+config = load_config("../config/config.toml")
 
 logging.basicConfig(
-    filename="../log/app.log",
-    level=logging.INFO,
+    filename=config["logging"]["path"],
+    level=config["logging"]["level"],
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
